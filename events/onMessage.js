1c7c8121a4513679ec4c4df34332131c64885264
CombustibleToast <34487106+CombustibleToast@users.noreply.github.com>
2024-05-19 15:49:26 -0400

    accepting message functions again. They now use a regex for trigger instead of a string match. also a package update.

--- events/onMessage.js
+++ events/onMessage.js
@@ -5,18 +5,25 @@ const { productionGuildId } = require('../secrets.json');
 module.exports = {
     name: Events.MessageCreate,
     execute(message){
-        return;
         //check if this iteration of the bot services this guild but allow all DMs
         if(message.guild != null && message.guild.id != productionGuildId){
-            console.log(`Received a message from another guild ${message.guild.id}`);
+            // console.log(`Received a message from another guild ${message.guild.id}`);
             return;
         }
+
         //check if message is from a bot
         if(message.author.bot)
             return;
-        console.log("got message");
+
+        const triggeredFunction = client.functions.find(func => func.trigger ? func.trigger.test(message.content) : false);
+
+        console.log(`func: ${triggeredFunction}`)
+        if(!triggeredFunction)
+            return;
+
+        console.log(`Processing message from ${message.author.username}: "${message.content}"`);
         try{
-            client.functions.find(func => func.trigger ? message.content == func.trigger : false).execute(message);
+            triggeredFunction.execute(message);
         }
         catch(e){
             console.error(`Couldn't act on a message event:\n${e.stack}`);
